e1249182dd0e430d31a9e92241df36d6ad9ae168
Vishnu PK <vishnu.pk@MacBook-Pro.local>
2022-02-09 17:59:48 +0530

    added back the CROS policy

--- app/main.py
+++ app/main.py
@@ -10,15 +10,19 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# origins = ["*"]
+origins = [
+    "http://www.google.com",
+    "https://www.google.com",
+    "http://www.google.com/",
+    "https://www.google.com/"]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins= origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
@@ -26,3 +30,6 @@ app.include_router(auth.router)
 app.include_router(votes.router)
 
 
+@app.get("/")
+def root():
+    return {"message": "welcome"}
